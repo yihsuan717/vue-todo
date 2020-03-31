e5d83a99ec4087dd262e5ed445bb46c1f629c5fc
yi.xuan <yihsuan717@gmail.com>
2020-03-31 23:13:07 +0800

    deploy

---
+++ precache-manifest.b997fbffb468ed5dfb99ab9369ae217f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92a981191f38accac458",
+    "url": "/vue-todo/css/app.ce38f044.css"
+  },
+  {
+    "revision": "4c32dc72dad82872e478",
+    "url": "/vue-todo/css/chunk-vendors.06337ae9.css"
+  },
+  {
+    "revision": "63a7d78d42c33b94fc7b957524795cac",
+    "url": "/vue-todo/img/logo.63a7d78d.svg"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/vue-todo/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "81aecefdb2ddccd87ab2206e0431d081",
+    "url": "/vue-todo/index.html"
+  },
+  {
+    "revision": "d27441a7eead1ed32c95",
+    "url": "/vue-todo/js/about.3c5cd7ff.js"
+  },
+  {
+    "revision": "92a981191f38accac458",
+    "url": "/vue-todo/js/app.1ad6f5cd.js"
+  },
+  {
+    "revision": "4c32dc72dad82872e478",
+    "url": "/vue-todo/js/chunk-vendors.b46eb984.js"
+  },
+  {
+    "revision": "fbafddbca0454e2cf83c4640f9391b35",
+    "url": "/vue-todo/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-todo/robots.txt"
+  }
+]);
